1377f46ba03b7dc7a3c5ce8a26bdcbce5d711d96
FabioBocci <noobgamingita@gmail.com>
2020-12-27 15:49:06 +0100

    Created RMI for Server

---
+++ NotifyEventInterface.java
@@ -0,0 +1,13 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface NotifyEventInterface extends Remote {
+
+    //notifica per cambio di stato di un utente.
+    public void notifyEventUser(String User,String Status)throws RemoteException;
+
+    //TODO Chat ecc...
+    //notifica per nuovi messaggi aggiunti
+    public void notifyEventChat(String User,String msg) throws RemoteException;
+
+}
