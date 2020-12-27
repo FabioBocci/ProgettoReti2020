08c0448b9a646bca51f0bf6d19115ee2845ab7ac
FabioBocci <noobgamingita@gmail.com>
2020-12-27 15:48:45 +0100

    created Client Main

---
+++ ClientMain.java
@@ -0,0 +1,19 @@
+import java.rmi.RemoteException;
+import java.rmi.server.RemoteObject;
+
+public class ClientMain extends RemoteObject implements NotifyEventInterface, ClientInterface {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public synchronized void notifyEventUser(String User, String Status) throws RemoteException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public synchronized void notifyEventChat(String User, String msg) throws RemoteException {
+        // TODO Auto-generated method stub
+
+    }
+    
+}
