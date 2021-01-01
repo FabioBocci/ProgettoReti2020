69ea97346550b64d00fcfc054aa2130cb3d07d3e
FabioBocci <noobgamingita@gmail.com>
2021-01-01 19:58:18 +0100

    chat

--- Tools/ChatThread.java
+++ Tools/ChatThread.java
@@ -6,50 +6,54 @@ import java.net.InetAddress;
 import java.net.MulticastSocket;
 
 public class ChatThread extends Thread {
-    final static int BUFFER_SIZE =4096;
+
+    final static int BUFFER_SIZE = 4096;
     private MessagingQueue queue;
-    private String IP;
+
+
     private int PORT;
     private MulticastSocket multicast;
+    private InetAddress group;
 
-    public ChatThread(MessagingQueue queue, String IP, int PORT) throws IOException
+    public ChatThread(MessagingQueue queue, MulticastSocket socket , InetAddress group , int port) throws IOException
     {
         this.queue=queue;
-        this.IP=IP;
-        this.PORT=PORT;
-        multicast =new MulticastSocket(this.PORT);
-        InetAddress group = InetAddress.getByName(this.IP);
-        multicast.joinGroup(group);
+        this.PORT=port;
+        this.multicast = socket;
+        this.group = group;
     }
 
+    public void sendMsg(String msg)throws NullPointerException, IOException
+    {
+        if(msg == null) throw new NullPointerException();
+        byte[] buffer = msg.getBytes();
+        DatagramPacket datagram = new DatagramPacket(buffer,buffer.length,this.group,this.PORT);
+        multicast.send(datagram);
+    }
+    
     public void addMessage(String msg)
     {
         queue.put(msg);
     }
-
+    
     public void recive() throws IOException
     {
         byte[] msg_recived= new byte[BUFFER_SIZE];
-        DatagramPacket PACK = new DatagramPacket(msg_recived, msg_recived.length);
+        DatagramPacket PACK = new DatagramPacket(msg_recived, msg_recived.length ,this.group,this.PORT);
         multicast.receive(PACK);
         String msg = new String(PACK.getData(),0,PACK.getLength(),"UTF-8");
+        
         addMessage(msg);
     }
-    public void send() throws IOException
-    {
-        byte[] msg_send = queue.getMsg().getBytes();
-        DatagramPacket PACK = new DatagramPacket(msg_send, msg_send.length);
-        multicast.send(PACK);
-    }
-
     @Override
     public void run() {
         try {
+            multicast.joinGroup(group);
             while(!Thread.interrupted())
             {
-                if(!queue.isSended()) send();
                 recive();
             }
+            multicast.leaveGroup(group);
             
         } catch (IOException e) {
             e.printStackTrace();
