d982c6cc092e29711e65731e498c1eb23194fc7f
FabioBocci <noobgamingita@gmail.com>
2021-01-21 19:15:17 +0100

    new exception

---
+++ Exceptions/ProjectNotFinishableException.java
@@ -0,0 +1,11 @@
+package Exceptions;
+
+public class ProjectNotFinishableException extends RuntimeException {
+    public ProjectNotFinishableException(String s)
+    {super(s);}
+    public ProjectNotFinishableException()
+    {
+        
+    }
+    
+}
